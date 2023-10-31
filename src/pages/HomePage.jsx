1a0c6edc7264bbf0d451d47a51a60f5e8dc837c6
Christian Walker <christiandwalker2021@gmail.com>
2023-10-31 01:20:56 -0500

    Added descriptors to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -65,6 +65,21 @@ export default function HomePage(){
                 </div>
                 <div name="how" className="how">
                     <h1>How Does It Work?</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus turpis massa tincidunt dui ut. Elementum pulvinar etiam non quam lacus suspendisse. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Adipiscing diam donec adipiscing tristique risus. Mi in nulla posuere sollicitudin aliquam. Mauris nunc congue nisi vitae. Tincidunt augue interdum velit euismod in pellentesque massa. Convallis aenean et tortor at. Condimentum mattis pellentesque id nibh tortor. Justo donec enim diam vulputate ut pharetra sit. Augue interdum velit euismod in pellentesque. Placerat duis ultricies lacus sed turpis tincidunt id. Aliquet risus feugiat in ante metus dictum.
+
+                </p>
+                    <p>
+
+                        Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Tortor posuere ac ut consequat. Pharetra massa massa ultricies mi quis hendrerit dolor magna eget. Netus et malesuada fames ac turpis egestas. Bibendum arcu vitae elementum curabitur vitae nunc. Viverra maecenas accumsan lacus vel facilisis volutpat est velit. Leo vel fringilla est ullamcorper eget nulla facilisi. Mauris nunc congue nisi vitae suscipit tellus. Nam at lectus urna duis. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Ornare quam viverra orci sagittis eu volutpat odio. Nunc id cursus metus aliquam eleifend mi. Magna fringilla urna porttitor rhoncus dolor purus non enim. Auctor augue mauris augue neque gravida in fermentum. Suspendisse interdum consectetur libero id faucibus. Elementum nisi quis eleifend quam adipiscing vitae proin. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Aliquam purus sit amet luctus venenatis.
+
+                    </p>
+                    <p>
+                        Massa enim nec dui nunc mattis enim ut tellus elementum. Enim neque volutpat ac tincidunt vitae semper quis lectus nulla. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Viverra aliquet eget sit amet tellus cras adipiscing enim. Nunc non blandit massa enim. Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Amet mauris commodo quis imperdiet massa tincidunt. Volutpat consequat mauris nunc congue nisi vitae suscipit. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Egestas maecenas pharetra convallis posuere morbi leo urna. Nisi lacus sed viverra tellus. Erat imperdiet sed euismod nisi porta lorem mollis. Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc. Feugiat nisl pretium fusce id velit ut tortor pretium. Dui id ornare arcu odio ut.
+                    </p>
+                    <p>
+                        Cras pulvinar mattis nunc sed blandit. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et ligula ullamcorper malesuada proin libero nunc consequat. Metus aliquam eleifend mi in nulla posuere sollicitudin. Maecenas sed enim ut sem viverra aliquet eget. Amet consectetur adipiscing elit ut. Vitae purus faucibus ornare suspendisse sed. Eget mauris pharetra et ultrices neque ornare aenean euismod. Enim tortor at auctor urna. Ut tristique et egestas quis ipsum. Sit amet venenatis urna cursus eget nunc. Pellentesque massa placerat duis ultricies lacus sed.
+                    </p>
                 </div>
             </div>
             }
